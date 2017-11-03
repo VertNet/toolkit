dd1cc3934fd8bda9410b67c6d58e1982b5b2a06d
tucotuco <gtuco.btuco@gmail.com>
2017-11-02 23:12:28 -0300

    Amended Taxon 2 query to set subspecies to null for null species. Substituted perl for sed in Purge processing to avoid memory errors for long lines.

--- PurgeNonprintingCharacters.sh
+++ PurgeNonprintingCharacters.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/bash
   f=$1  
   # Change new line followed by "PhysicalObject or "Event or "Sound or "StillImage or "MovingImage to сс"PhysicalObject or сс"Event or сс"Sound or сс"StillImage or сс"MovingImage, respectively. These should be the beginnings of records.
-  sed -e ':a' -e 'N' -e '$!ba' -e 's/.\n\"PhysicalObject/сс\"PhysicalObject/g' $f>$f.1
-  sed -e ':a' -e 'N' -e '$!ba' -e 's/.\n\"Event/сс\"Event/g' $f.1>$f.2
+  perl -pe 's/.\n\"PhysicalObject/сс\"PhysicalObject/g' $f>$f.1
+#  sed -e ':a' -e 'N' -e '$!ba' -e 's/.\n\"PhysicalObject/сс\"PhysicalObject/g' $f>$f.1
+  perl -pe 's/.\n\"Event/сс\"Event/g' $f.1>$f.2
+#  sed -e ':a' -e 'N' -e '$!ba' -e 's/.\n\"Event/сс\"Event/g' $f.1>$f.2
   rm $f.1
-  sed -e ':a' -e 'N' -e '$!ba' -e 's/.\n\"Sound/сс\"Sound/g' $f.2>$f.3
+  perl -pe 's/.\n\"Sound/сс\"Sound/g' $f.2>$f.3
+#  sed -e ':a' -e 'N' -e '$!ba' -e 's/.\n\"Sound/сс\"Sound/g' $f.2>$f.3
   rm $f.2
-  sed -e ':a' -e 'N' -e '$!ba' -e 's/.\n\"StillImage/сс\"StillImage/g' $f.3>$f.4
+  perl -pe 's/.\n\"StillImage/сс\"StillImage/g' $f.3>$f.4
+#  sed -e ':a' -e 'N' -e '$!ba' -e 's/.\n\"StillImage/сс\"StillImage/g' $f.3>$f.4
   rm $f.3
-  sed -e ':a' -e 'N' -e '$!ba' -e 's/.\n\"MovingImage/сс\"MovingImage/g' $f.4>$f.5
+  perl -pe 's/.\n\"MovingImage/сс\"MovingImage/g' $f.4>$f.5
+#  sed -e ':a' -e 'N' -e '$!ba' -e 's/.\n\"MovingImage/сс\"MovingImage/g' $f.4>$f.5
   rm $f.4
-  sed -e ':a' -e 'N' -e '$!ba' -e 's/.\n\"Text/сс\"Text/g' $f.5>$f.6
+  perl -pe 's/.\n\"Text/сс\"Text/g' $f.5>$f.6
+#  sed -e ':a' -e 'N' -e '$!ba' -e 's/.\n\"Text/сс\"Text/g' $f.5>$f.6
   rm $f.5
   # Change final new line to сс also.
   perl -p -e 's/\r\n$/сс/ if eof' $f.6 > $f.7
