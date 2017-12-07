c2914a375ab8ce4062640a94a2ee401e9f68823f
tucotuco <tuco@berkeley.edu>
2017-12-07 14:12:12 -0300

    Made csv output go to SimpleDwCForIPT.csv rather than SimpleDwCForVertNet.csv.

--- 1c - RunAggregators.bat
+++ 1c - RunAggregators.bat
@@ -58,26 +58,26 @@ REM script.
 %accessexe% %CD%\workspace\Aggregator.mdb /x "Aggregate and Export"
 
 REM Remove previous outputs from the folder
-del ForIPT\SimpleDwCForVertNet.zip
-del ForIPT\SimpleDwCForVertNet.csv.gz
-del ForIPT\SimpleDwCForVertNet.zip
+del ForIPT\SimpleDwCForIPT.zip
+del ForIPT\SimpleDwCForIPT.csv.gz
+del ForIPT\SimpleDwCForIPT.zip
 
 REM Run script to prepend UTF8 Byte Order Marker bytes.
-gawk -f utf8er.awk ForIPT\SimpleDwCForVertNet.csv>ForIPT\testout.csv
-del ForIPT\SimpleDwCForVertNet.csv
-del ForIPT\SimpleDwCForVertNetPrepurge.csv
-move ForIPT\testout.csv ForIPT\SimpleDwCForVertNetPrepurge.csv
+gawk -f utf8er.awk ForIPT\SimpleDwCForIPT.csv>ForIPT\testout.csv
+del ForIPT\SimpleDwCForIPT.csv
+del ForIPT\SimpleDwCForIPTPrepurge.csv
+move ForIPT\testout.csv ForIPT\SimpleDwCForIPTPrepurge.csv
 
 REM Run scripts to report potential errors in the output file.
-gawk -f EncodingIssues.awk ForIPT\SimpleDwCForVertNetPrepurge.csv>ForIPT\EncodingIssues.txt
-gawk -f NewLineIssues.awk ForIPT\SimpleDwCForVertNetPrepurge.csv>ForIPT\NewLineIssues.txt
-gawk -f BlankLineIssues.awk ForIPT\SimpleDwCForVertNetPrepurge.csv>ForIPT\BlankLineIssues.txt
+gawk -f EncodingIssues.awk ForIPT\SimpleDwCForIPTPrepurge.csv>ForIPT\EncodingIssues.txt
+gawk -f NewLineIssues.awk ForIPT\SimpleDwCForIPTPrepurge.csv>ForIPT\NewLineIssues.txt
+gawk -f BlankLineIssues.awk ForIPT\SimpleDwCForIPTPrepurge.csv>ForIPT\BlankLineIssues.txt
 
 REM Run script to remove non-printing control characters \r \n \f \v \t in data fields.
-REM Note that large files may cause out of memory errors resulting in an empty SimpleDwCForVertNet.csv file.
+REM Note that large files may cause out of memory errors resulting in an empty SimpleDwCForIPT.csv file.
 REM split the file into parts of 100000 lines or less each and purge them separately.
 REM Set to split on 100k records by default so that this only has to be edited if a memory error is encountered.
-split -l 100000 ForIPT\SimpleDwCForVertNetPrePurge.csv ForIPT\part
+split -l 100000 ForIPT\SimpleDwCForIPTPrePurge.csv ForIPT\part
 sh PurgeNonprintingCharacters.sh ForIPT\partaa
 sh PurgeNonprintingCharacters.sh ForIPT\partab
 sh PurgeNonprintingCharacters.sh ForIPT\partac
@@ -89,24 +89,24 @@ sh PurgeNonprintingCharacters.sh ForIPT\partah
 sh PurgeNonprintingCharacters.sh ForIPT\partai
 sh PurgeNonprintingCharacters.sh ForIPT\partaj
 cd ForIPT
-cat partaa partab partac partad partae partaf partag partah partai partaj> SimpleDwCForVertNet.csv
-REM cat partaa > SimpleDwCForVertNet.csv
+cat partaa partab partac partad partae partaf partag partah partai partaj> SimpleDwCForIPT.csv
+REM cat partaa > SimpleDwCForIPT.csv
 rm parta*
-diff SimpleDwCForVertNetPrepurge.csv SimpleDwCForVertNet.csv > purge_diffs.txt
+diff SimpleDwCForIPTPrepurge.csv SimpleDwCForIPT.csv > purge_diffs.txt
 cd ..
 copy ForIPT\purge_diffs.txt reports\Report-LinesWithNonprintingCharacters.txt
-REM copy ForIPT\simpledwcforvertnetprepurge.csv ForIPT\SimpleDwCForVertNet.csv
-REM sh PurgeNonprintingCharacters.sh ForIPT\SimpleDwCForVertNet.csv
+REM copy ForIPT\SimpleDwCForIPTprepurge.csv ForIPT\SimpleDwCForIPT.csv
+REM sh PurgeNonprintingCharacters.sh ForIPT\SimpleDwCForIPT.csv
 
 REM Run NewLine reporting script again after purging new lines and carriage returns in data
-gawk -f NewLineIssues.awk ForIPT\SimpleDwCForVertNet.csv>ForIPT\NewLineIssuesAfterCRLFRemoval.txt
+gawk -f NewLineIssues.awk ForIPT\SimpleDwCForIPT.csv>ForIPT\NewLineIssuesAfterCRLFRemoval.txt
 
 REM Remove the working copy of the Aggregator
 mv workspace\Aggregator.mdb bkp\Aggregator.mdb
 
-REM Archive the resulting SimpleDwCForVertNet file with gzip.
+REM Archive the resulting SimpleDwCForIPT file with gzip.
 REM cd ForIPT
-REM gzip SimpleDwCForVertNet.csv
+REM gzip SimpleDwCForIPT.csv
 REM cd ..
 
 :end
